86b560963045dea9bf5ec651052643b27046d5d7
Mike15254 <mike8max10@gmail.com>
2023-01-23 22:49:06 +0300

    intial commit

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import { debounce } from 'lodash';
+
+$(document).ready(() => {
+  $('body').prepend('<div id="logo"><div>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click the here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('button').click(debounceUpdateCounter);
+  $('body').append('<p>Copyright - Holberton School</p>');
+});
+
+let count = 0;
+
+function updateCounter() {
+  count++;
+  $('#count').text(`${count} clicks on the button`);
+}
+
+const debounceUpdateCounter = debounce(updateCounter, 500);
