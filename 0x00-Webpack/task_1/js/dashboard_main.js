2e70302f82a24087d3220a8b61483be90092e4e9
Mike15254 <mike8max10@gmail.com>
2023-01-24 21:50:06 +0300

    intial commit

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -6,13 +6,13 @@ $(document).ready(() => {
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id="count"></p>');
-  $('button').click(updateCounter);
+  $('button').on('click', _.debounce(updateCounter));
   $('body').append('<p>Copyright - Holberton School');
 });
 
 let count = 0;
 
-const updateCounter = _.debounce(() => {
+function updateCounter() {
   count++;
-  document.getElementById('count').innerHTML = `${count} clicks on the button`;
-});
+  $('#count').text(`${count} clicks on the button`);
+}
