0c4392df7d012ca3535a32896dfd758b2a5311c7
Mike15254 <mike8max10@gmail.com>
2023-01-24 21:54:48 +0300

    intial commit

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,18 +1,19 @@
 import $ from 'jquery';
 import _ from 'lodash';
+$(function () {
+  let count = 0;
 
-$(document).ready(() => {
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id="count"></p>');
-  $('button').on('click', _.debounce(updateCounter, 100));
-  $('body').append('<p>Copyright - Holberton School');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  // $('button').on('click', updateCounter);
+  $('button').on('click', _.debounce(updateCounter, 500));
+
+  function updateCounter() {
+    count = count + 1;
+    $('#count').text(count + ' clicks on the button');
+  }
 });
-
-let count = 0;
-
-function updateCounter() {
-  count++;
-  $('#count').text(`${count} clicks on the button`);
-}
