68c1e9d51920da03d155fc950fa4657c72260618
Mike15254 <mike8max10@gmail.com>
2023-01-26 21:09:27 +0300

    Fix: fixed some bugs

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import logo from './holberton-logo.jpg';
+import React, { useState } from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <div class="horizontal-line"></div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">
+        <div class="horizontal-line"></div>
+        <p>
+          <em>Copyright - Holberton School</em>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
