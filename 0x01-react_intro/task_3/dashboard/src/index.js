5103cf85e0e2e6a375ec45724018a9159ec8b5b4
Mike15254 <mike8max10@gmail.com>
2023-01-28 22:36:31 +0300

    File: intial commit

--- 0x01-react_intro/task_3/dashboard/src/index.js
+++ 0x01-react_intro/task_3/dashboard/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './App.css';
+import './Notifications.css';
 import App from './App';
 import { Notifications } from './Notifications';
 
