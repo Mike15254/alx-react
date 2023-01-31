ad3c41c9836ce3194876b28e6d7dccbb4d7dcc61
Mike15254 <mike8max10@gmail.com>
2023-01-31 11:47:49 +0300

    File: inital commit

---
+++ 0x02-react_props/task_0/dashboard/src/Header/Header.js
@@ -0,0 +1,16 @@
+import logo from '../assets/holberton-logo.jpg';
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} alt="Holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
