b09d0f27bc9acd5d03aa2edc34c51c6659eef214
Mike15254 <mike8max10@gmail.com>
2023-02-04 23:43:31 +0300

    Fix: bug fix

--- 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -1,40 +1,40 @@
 import React from 'react';
 import closeIcon from '../assets/close-icon.png';
 import { getLatestNotification } from '../utils/utils';
+import NotificationItem from './NotificationItem';
 import './Notifications.css';
 
-export function Notifications() {
+const Notifications = () => {
   return (
     <div className="Notifications">
-      <button
-        style={{
-          color: '#3a3a3a',
-          fontWeight: 'bold',
-          background: 'none',
-          border: 'none',
-          fontSize: '15px',
-          position: 'absolute',
-          right: '9px',
-          top: '9px',
-          margin: '2rem',
-          cursor: 'pointer',
-        }}
-        aria-label="Close"
-        onClick={(e) => {
-          console.log('Close button has been clicked');
-        }}
-      >
-        <img src={ closeIcon } alt="closeicon" />
-      </button>
       <p>Here is the list of notifications</p>
       <ul>
-        <li data-priority="default">New course available</li>
-        <li data-priority="urgent"> New resumw available</li>
-        <li
-          data-priority="urgent"
-          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
-        ></li>
+        <NotificationItem type="default" value="New course available" />
+        <NotificationItem type="urgent" value="New resume available" />
+        <NotificationItem
+          type="urgent"
+          html={{ __html: getLatestNotification() }}
+        />
       </ul>
+      <button
+        type="button"
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+        style={{
+          display: 'inline-block',
+          position: 'absolute',
+          top: '16px',
+          right: '16px',
+          background: 0,
+          border: 0,
+          outline: 'none',
+          cursor: 'pointer',
+        }}
+      >
+        <img src={closeIcon} alt="" style={{ width: '8px', height: '8px' }} />
+      </button>
     </div>
   );
-}
+};
+
+export default Notifications;
