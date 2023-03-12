a45675a5446aa8f92c78f7de4fd24d19fa275be4
Mike15254 <mike8max10@gmail.com>
2023-03-12 23:09:29 +0300

    Commit: Add NotificationItem component with conditionally rendered HTML li element

--- 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_4/dashboard/src/Notifications/NotificationItem.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// A function declaration that takes three arguments 'type', 'html', and 'value' 
 function NotificationItem({ type, html, value }) {
+  // Declare a variable li
   let li;
 
-  value
-    ? (li = <li data-priority='default' data-notification-type={type}>{value}</li>)
-    : (li = (
-        <li data-priority='urgent' data-notification-type={type} dangerouslySetInnerHTML={html}></li>
-      ));
+  // If the value is not null, assigns an HTML li item with 'data-priority' attribute set to 'default', 'data-notification-type' set to the passed in 'type' and display its 'value'
+  value ? (li = <li data-priority='default' data-notification-type={type}>{value}</li>)
 
+  //If the value attribute is null, assigns an HTML li item with 'data-priority' attribute set to 'urgent', 'data-notification-type' set to the passed in 'type', and displays the raw HTML from the 'html' attribute. It must be used cautiously because it can expose your application to XSS vulnerabilities.
+    : (li = (<li data-priority='urgent' data-notification-type={type} dangerouslySetInnerHTML={html}></li>));
+
+  // Return the final HTML element constructed based on input values
   return li;
 }
+
 
 NotificationItem.defaultProps = {
   type: 'default',
